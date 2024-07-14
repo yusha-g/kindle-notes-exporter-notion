97790b27802ff9f053114533c9ebfa3ddff47d22
pratyusha <pratyusha.gh@gmail.com>
2024-07-14 22:51:46 +0530

    chore: jot down initial algo idea

---
+++ src/kindle_exporter/exporter.py
@@ -0,0 +1,9 @@
+"""
+# IDEA: intial Algorithm
+- Read My Clippings and clean-up data. Info details:
+    - Only non-parathesised strings are book names.
+    - The string inside last paranthesis () is Author Name
+    - Based on Author Name, make sure book name only contains name.
+    - Clean up book_name: remove _
+- Each book instance has to be unqiue by it's name
+"""
